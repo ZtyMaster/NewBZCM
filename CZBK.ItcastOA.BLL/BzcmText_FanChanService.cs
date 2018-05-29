b43aec4846f18edda65e35c1f63df37f53af330e
Zty <walplrd1314@qq.com>
2018-05-29 15:44:09 +0800

    zty

--- CZBK.ItcastOA.BLL/BzcmText_FanChanService.cs
+++ CZBK.ItcastOA.BLL/BzcmText_FanChanService.cs
@@ -18,25 +18,24 @@ namespace CZBK.ItcastOA.BLL
             /// </summary>
             /// <param name="userInfoSearchParam"></param>
             /// <returns></returns>
-            
-            
-            var temp = this.GetCurrentDbSession.BzcmText_FanChanDal.LoadEntities(c => c.DEL == (astr.IsMaster?1: 0));
-            
+
+
+            var temp = this.GetCurrentDbSession.BzcmText_FanChanDal.LoadEntities(c => c.DEL == (astr.IsMaster ? 1 : 0));
+
             if (astr.IsTop) {
-                temp = temp.Where(x => x.IsTop == 1&&x.IsTopStartTime<=DateTime.Now&&x.IsTopStopTime>=DateTime.Now);
+                temp = temp.Where(x => x.IsTop == 1 && x.IsTopStartTime <= DateTime.Now && x.IsTopStopTime >= DateTime.Now);
             }
-           
+
             if (!string.IsNullOrEmpty(astr.Items))
             {
                 int id = Convert.ToInt32(astr.Items);
-                temp = temp.Where<BzcmText_FanChan>(u => u.IsFristItemsID==id);
+                temp = temp.Where<BzcmText_FanChan>(u => u.IsFristItemsID == id);
             }
             astr.TotalCount = temp.Count();
-            var temps= temp.OrderBy<BzcmText_FanChan, long>(u => u.ID).Skip<BzcmText_FanChan>((astr.PageIndex - 1) * astr.PageSize).Take<BzcmText_FanChan>(astr.PageSize);
+            var temps = temp.OrderBy<BzcmText_FanChan, long>(u => u.ID).Skip<BzcmText_FanChan>((astr.PageIndex - 1) * astr.PageSize).Take<BzcmText_FanChan>(astr.PageSize);
             var temp_bzcm = this.GetCurrentDbSession.BZCMLouPanJianJieDal.LoadEntities(x => x.DEL == 0).DefaultIfEmpty();
-            var ret = from a in temps 
-                      from b in temp_bzcm 
-                      select  new BzcmClass 
+            var ret = from a in temps
+                      select new BzcmClass
                       {
                           ID = a.ID,
                           Addtime = a.Addtime,
@@ -65,14 +64,23 @@ namespace CZBK.ItcastOA.BLL
                           Str_Image = a.Str_Image,
                           Str_Name = a.Str_Name,
                           Str_Photo = a.Str_Photo,
-                          Shtere = b.BzcmTextID == a.ID ? true : false
-                       } ;
-            var tmp = ret.GroupBy(x => x.ID).Where(x => x.Count() > 1).ToList(); 
+                          
+                      };
+
+            //from b in temp_bzcm
+            //Shtere = b.BzcmTextID == a.ID ? true : false
+            List<BzcmClass> lbc = new List<BzcmClass>();
+            foreach (var item in ret) {
+                item.Shtere = temp_bzcm.Where(x => x.BzcmTextID == item.ID).Count() > 0 ? true : false;
+                lbc.Add(item);
+            }
             
-            return ret.ToList();
+            return lbc.ToList();
 
         }
     }
-       
-    
+  
+
+
+
 }
